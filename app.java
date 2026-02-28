ac654c4e8b839586e9c013608a4e74b606f186c3
sanjana631 <sanjanadass631@gmail.com>
2026-02-28 12:01:07 +0530

    Add sum calculation and output to app.java

--- app.java
+++ app.java
@@ -2,9 +2,9 @@ public class app {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-
+        int sum= a+b ;
         int diff = a - b;
-
+        System.out.println("Sum = " + sum);
         System.out.println("Diff = " + diff);
     }
 }
