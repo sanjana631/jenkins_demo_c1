26beb8af0d7e5c4e9b3bd9d136e4a68cc7607aff
sanjana631 <sanjanadass631@gmail.com>
2026-02-28 11:58:36 +0530

    Calculate and print the difference between two numbers

--- app.java
+++ app.java
@@ -3,8 +3,8 @@ public class app {
         int a = 10;
         int b = 20;
 
-        int sum = a + b;
+        int diff = a - b;
 
-        System.out.println("Sum = " + sum);
+        System.out.println("Diff = " + diff);
     }
 }
